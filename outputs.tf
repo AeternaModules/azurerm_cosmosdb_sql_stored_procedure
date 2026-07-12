3cd8f73cf3fef99e60a832df49b208fa6a7bb29f
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:16:42 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,29 +1,25 @@
-output "cosmosdb_sql_stored_procedures" {
-  description = "All cosmosdb_sql_stored_procedure resources"
-  value       = azurerm_cosmosdb_sql_stored_procedure.cosmosdb_sql_stored_procedures
-}
 output "cosmosdb_sql_stored_procedures_account_name" {
-  description = "List of account_name values across all cosmosdb_sql_stored_procedures"
-  value       = [for k, v in azurerm_cosmosdb_sql_stored_procedure.cosmosdb_sql_stored_procedures : v.account_name]
+  description = "Map of account_name values across all cosmosdb_sql_stored_procedures, keyed the same as var.cosmosdb_sql_stored_procedures"
+  value       = { for k, v in azurerm_cosmosdb_sql_stored_procedure.cosmosdb_sql_stored_procedures : k => v.account_name }
 }
 output "cosmosdb_sql_stored_procedures_body" {
-  description = "List of body values across all cosmosdb_sql_stored_procedures"
-  value       = [for k, v in azurerm_cosmosdb_sql_stored_procedure.cosmosdb_sql_stored_procedures : v.body]
+  description = "Map of body values across all cosmosdb_sql_stored_procedures, keyed the same as var.cosmosdb_sql_stored_procedures"
+  value       = { for k, v in azurerm_cosmosdb_sql_stored_procedure.cosmosdb_sql_stored_procedures : k => v.body }
 }
 output "cosmosdb_sql_stored_procedures_container_name" {
-  description = "List of container_name values across all cosmosdb_sql_stored_procedures"
-  value       = [for k, v in azurerm_cosmosdb_sql_stored_procedure.cosmosdb_sql_stored_procedures : v.container_name]
+  description = "Map of container_name values across all cosmosdb_sql_stored_procedures, keyed the same as var.cosmosdb_sql_stored_procedures"
+  value       = { for k, v in azurerm_cosmosdb_sql_stored_procedure.cosmosdb_sql_stored_procedures : k => v.container_name }
 }
 output "cosmosdb_sql_stored_procedures_database_name" {
-  description = "List of database_name values across all cosmosdb_sql_stored_procedures"
-  value       = [for k, v in azurerm_cosmosdb_sql_stored_procedure.cosmosdb_sql_stored_procedures : v.database_name]
+  description = "Map of database_name values across all cosmosdb_sql_stored_procedures, keyed the same as var.cosmosdb_sql_stored_procedures"
+  value       = { for k, v in azurerm_cosmosdb_sql_stored_procedure.cosmosdb_sql_stored_procedures : k => v.database_name }
 }
 output "cosmosdb_sql_stored_procedures_name" {
-  description = "List of name values across all cosmosdb_sql_stored_procedures"
-  value       = [for k, v in azurerm_cosmosdb_sql_stored_procedure.cosmosdb_sql_stored_procedures : v.name]
+  description = "Map of name values across all cosmosdb_sql_stored_procedures, keyed the same as var.cosmosdb_sql_stored_procedures"
+  value       = { for k, v in azurerm_cosmosdb_sql_stored_procedure.cosmosdb_sql_stored_procedures : k => v.name }
 }
 output "cosmosdb_sql_stored_procedures_resource_group_name" {
-  description = "List of resource_group_name values across all cosmosdb_sql_stored_procedures"
-  value       = [for k, v in azurerm_cosmosdb_sql_stored_procedure.cosmosdb_sql_stored_procedures : v.resource_group_name]
+  description = "Map of resource_group_name values across all cosmosdb_sql_stored_procedures, keyed the same as var.cosmosdb_sql_stored_procedures"
+  value       = { for k, v in azurerm_cosmosdb_sql_stored_procedure.cosmosdb_sql_stored_procedures : k => v.resource_group_name }
 }
 
